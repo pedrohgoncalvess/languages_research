5f9482d706766ac315003f6a4ce0f8255a9375bb
Pedro H <pedro_gonsalves@hotmail.com>
2023-05-15 23:46:41 -0300

    add: query to get max id_repo

--- src/main/scala/database/operations.scala
+++ src/main/scala/database/operations.scala
@@ -1,7 +1,9 @@
 package database
 
 import java.util.concurrent.Executors
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.util.{Failure, Success}
 
 object PrivateExecutionContext {
   val executor = Executors.newFixedThreadPool(4)
@@ -29,5 +31,15 @@ object operations {
         println(s"Query failed after $numTries tries, reason: $ex")
         Future.failed(ex)
     }
+  }
+
+  def maxIDRepositorie: Option[Long] = {
+    val table = Tables.repositoriesTable.map(_.id_repo)
+    val max = table.max
+
+    val result = connection.db.run(max.result)
+    Await.result(result, 5.seconds)
+
+    result.value.get.get
   }
 }
