893f694dce3c7fc766a872565cb997aa79dab6d8
Pedro H <pedro_gonsalves@hotmail.com>
2023-05-30 18:10:00 -0300

    add: tags and languages table

--- src/main/resources/db/migration/V1__create_repo_table.sql
+++ src/main/resources/db/migration/V1__create_repo_table.sql
@@ -11,3 +11,23 @@ create table if not exists github.repositories (
     inserted_at datetime default current_timestamp,
         primary key (id, id_repo)
 );
+
+create table if not exists github.repositories_tags(
+    id int auto_increment,
+    id_compost varchar(70) unique,
+    id_repo int,
+    tag varchar(50),
+        primary key (id, id_compost),
+            foreign key (id_repo) references github.repositories(id_repo)
+);
+
+create table if not exists github.repositories_languages(
+    id int auto_increment,
+    id_compost varchar(70) unique,
+    id_repo int,
+    language varchar(50),
+    use_language int,
+    representativity double,
+        primary key (id, id_compost),
+            foreign key (id_repo) references github.repositories(id_repo)
+);
