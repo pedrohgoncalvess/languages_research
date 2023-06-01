fd0238284e4d86db70b8c8048efea9675e367b0e
Pedro H <pedro_gonsalves@hotmail.com>
2023-06-01 01:33:53 -0300

    fix: migration, application.conf values

--- src/main/scala/database/migration.scala
+++ src/main/scala/database/migration.scala
@@ -1,7 +1,17 @@
 package database
 
+
+
+
+
 object DatabaseMigration{
   import org.flywaydb.core.Flyway
+  import com.typesafe.config.ConfigFactory
+
+  private val config = ConfigFactory.load()
+  private val user = config.getString("LOGIN_DATABASE")
+  private val password = config.getString("PASSWORD_DATABASE")
+
   val flyway = Flyway.configure.dataSource("jdbc:mysql://localhost:3306/languages_search", "root", "fodao002").load()
 }
 
